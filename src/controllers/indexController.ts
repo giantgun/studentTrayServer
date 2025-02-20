18f84c2d1009803a345d3fc026947b56dfd954c5
giantgun <120796901+giantgun@users.noreply.github.com>
2025-02-20 13:35:37 +0100

    landingpage update

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,9 +1,31 @@
+import { PrismaClient } from "@prisma/client";
 import { NextFunction, Response, Request } from "express";
+import { shuffleArray } from "../utils/utils";
+
+const prisma = new PrismaClient();
 
 export async function index_get(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  res.json("");
+  let items = shuffleArray(await prisma.item.findMany({ take: 10, include: {item_school: {
+    include: {
+      school: true
+    }
+  }} })).slice(0, 2)
+  let services = shuffleArray(await prisma.service.findMany({ take: 10, include: {service_school: {
+    include: {
+      school: true
+    }
+  }} })).slice(0, 2)
+  let lodges = shuffleArray(await prisma.lodge.findMany({ take: 10 })).slice(0, 2)
+  let rooms = shuffleArray(await prisma.room.findMany({ take: 10 })).slice(0, 2)
+
+  res.json({
+    items,
+    services,
+    lodges,
+    rooms
+  });
 }
